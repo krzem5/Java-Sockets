a70e84f744977554fc9d55a8b26c4bd15890241e
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 13:48:57 +0100

    Push Update

---
+++ index.bat
@@ -0,0 +1,8 @@
+echo off
+echo NUL>_.class&&del /s /f /q *.class
+cls
+javac com/krzem/sockets/Main.java
+javac com/krzem/sockets/Client.java
+start /max cmd /c "java com/krzem/sockets/Client 192.168.178.56:8080"
+java com/krzem/sockets/Main 8080
+start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"

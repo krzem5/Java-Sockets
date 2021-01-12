ce68d2b8722aaf33b94335e238ea418ad08e9923
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:28:05 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -6,3 +6,4 @@ javac com/krzem/sockets/Client.java
 start /max cmd /c "java com/krzem/sockets/Client 192.168.178.56:8080"
 java com/krzem/sockets/Main 8080
 start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+
